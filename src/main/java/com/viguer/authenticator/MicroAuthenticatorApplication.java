1bbab5b2cf881ebad92a7f6ab3fab45381333f12
vincetl.guerrero <vincetl.guerrero@carroya.com>
2023-12-08 16:41:33 -0500

    Step-0001: Entities creation and DB H2

--- src/main/java/com/viguer/authenticator/MicroAuthenticatorApplication.java
+++ src/main/java/com/viguer/authenticator/MicroAuthenticatorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthenticatorApplication {
+public class MicroAuthenticatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthenticatorApplication.class, args);
+		SpringApplication.run(MicroAuthenticatorApplication.class, args);
 	}
 
 }
